663ee8fb40e441a739dcbed107a4a21234941dc1
今吉恒介 <ikkorokoro@icloud.com>
2021-05-29 19:31:18 +0900

    スコープとコンテキスト

--- 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
+++ 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
@@ -5,17 +5,21 @@
 let a = 0;
 var b = 0;
 function c() {}
+console.log(b);
 //ブラウザ -> 検証 -> sources -> debubber起動させる -> Scope確認
 
 // let,constはスクリプトスコープ
-/* var,functionはグローバルオブジェクトのWindowオブジェクトの中に値が格納されている。
-これがグローバルスコープ
+/* var,functionはグローバルオブジェクトのWindowオブジェクトの中に値が格納されている。これがグローバルスコープ
 
 要するにwindowオブジェクト ＝ グローバルスコープである 
 windowオブジェクトはデータを取得する際windowを省略することができる
 一般的は使い勝手は変わらないためスクリプトスコープもまとめてグローバルスコープと呼ばれることがある
 だがグローバルスコープ、スクリプトスコープの取れる値によって実行結果が変わる可能性があることを認識しておく
 */
+
+window.d = 1;
+let d = 2;
+console.log(d);
 
 console.log(window.b);  
 // 以下のようにwindowを省略できる
