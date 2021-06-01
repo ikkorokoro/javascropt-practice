acb31cbb3ab1653961a1801e4882333ec6d20c02
今吉恒介 <ikkorokoro@icloud.com>
2021-06-01 10:40:51 +0900

    クロージャー(動的な関数)

---
+++ 030_スコープ/060_クロージャー（動的な関数生成）/start/main.js
@@ -0,0 +1,21 @@
+// クロージャー（動的な関数)
+/* addNumberFactory(5);の引数5によって、addNumberのnumに５がセットされる状態になる。
+addNumberはaddNumberFactoryの中に記述されているためレキシブルスコープでnumを参照することができるため5がセットされる
+関数の引数の値によって生成される関数を変えることができるため、動的な関数として使用できる
+*/
+
+
+function addNumberFactory(num) {
+  function addNumber(value) {
+    return num + value
+  }
+  return addNumber
+}
+
+const add5 = addNumberFactory(5);
+const res = add5(10);
+console.log(res);
+// 引数を変更することで出力を変更することができる
+const add10 = addNumberFactory(10);
+const result = add10(15);
+console.log(result);
